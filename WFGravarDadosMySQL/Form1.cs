0c6db68e9389da77ea000698aa0e1be0321c0175
VINICIUS ANDRE FERREIRA LUIZ <vinicius.afluiz@senacsp.edu.br>
2023-07-19 15:25:26 -0300

    Projeto 19-07

--- WFGravarDadosMySQL/Form1.cs
+++ WFGravarDadosMySQL/Form1.cs
@@ -7,15 +7,46 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MySql.Data.MySqlClient;
 
 namespace WFGravarDadosMySQL
 {
     public partial class Form1 : Form
     {
+        MySqlConnection Conexao;
+
         public Form1()
         {
             InitializeComponent();
         }
 
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string data_source = "datasource=localhost;username=root;password=;database=db_agenda;";
+                Conexao = new MySqlConnection(data_source);
+
+                string sql = "Insert into contato (nome,email,telefone) Values('"+txtNome.Text+"', '"+txtEmail.Text+"', +'"+txtTelefone.Text+"')";
+
+                MySqlCommand comando = new MySqlCommand(sql, Conexao);
+
+                Conexao.Open();
+
+                comando.ExecuteReader();
+
+                MessageBox.Show("Cadastro inserido com sucesso");
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+
+            }
+            finally
+            {
+                Conexao.Close();
+            }
+        }
     }
 }
